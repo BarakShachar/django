ee0151207a03d95db279868d022dc0474a23772d
Barak Shachar <baraks@omnitelecom.co.il>
2023-04-08 15:02:21 +0300

    a

--- shifter/urls.py
+++ shifter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, TeamView
 
 urlpatterns = [
     path("user/", UserView.as_view()),
+    path("team/", TeamView.as_view()),
 ]
--- shifter/views.py
+++ shifter/views.py
@@ -14,3 +14,10 @@ class UserView(APIView):
         ):
             return Response({"key": "abcd"})
         return Response({"message": "username does not exist"}, status=400)
+
+
+class TeamView(APIView):
+    def get(self, request):
+        return Response(
+            {[{"name": "barak", "age": "26"}, {"nama": "sabrina"}]}, status=200
+        )
