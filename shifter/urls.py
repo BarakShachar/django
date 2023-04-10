6f4e4f22b35f282de585b199123e3d360028247a
Barak Shachar <baraks@omnitelecom.co.il>
2023-04-10 13:01:17 +0300

    a

--- shifter/urls.py
+++ shifter/urls.py
@@ -4,5 +4,5 @@ from .views import UserView, TeamView, ShiftsView
 urlpatterns = [
     path("user/", UserView.as_view()),
     path("team/", TeamView.as_view()),
-    path("shifts/<id:str>", TeamView.as_view()),
+    path("shifts/<int:id>/", TeamView.as_view()),
 ]
